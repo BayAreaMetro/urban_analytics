3ebaef36df53c822bd2579397477a8bbbef0486a
Michael Reilly <mreilly@mtc.ca.gov>
2015-07-24 14:37:19 -0700

    bringing in price transforms

--- bayarea_hedonic/bayarea_hedonic.R
+++ bayarea_hedonic/bayarea_hedonic.R
@@ -1,21 +1,45 @@
 redfin_raw = read.csv("redfin_03feb14.csv", header = TRUE)
+inflation = read.csv("inflation_cycle_adjust.csv", header = TRUE)
 
-redfin_sub <- subset(redfin_raw, Saleyear > 2011 & YearBuilt > 0 & Sqft > 0, select = c(LastSalePr, City, Sqft, YearBuilt, Beds))
+redfin_sub <- subset(redfin_raw, Saleyear > 1990 & YearBuilt > 0 & Sqft > 0, select = c(LastSalePr, Saleyear, City, Sqft, YearBuilt, Beds))
+infl <- subset(inflation, saleyear > 1990)
+homes <- merge (x = redfin_sub, y = infl, by.x = "Saleyear", by.y = "saleyear") 
 
-redfin_sub$price <- redfin_sub$LastSalePr / 100000
-redfin_sub$juriscat <- ifelse(redfin_sub$City == "San Francisco", c("SF"), c("Not SF")) 
+# add county var and then use below to do appropriate transform by county
+if(client=='private'){
+  tot.price <- net.price * 1.12
+} else if(client=='public'){
+  tot.price <- net.price * 1.06
+} else {
+  tot.price <- net.price
+}
+
+
+homes$price_cpi <- homes$LastSalePr * homes$adjcpi2010 /100000
+homes$price_hpi <- homes$LastSalePr * homes$adjHPIsfrsmt /100000
+
+homes$price <- homes$LastSalePr / 100000
+homes$juriscat <- ifelse(homes$City == "San Francisco", c("SF"), c("Not SF")) 
 #(tmp <- yes; tmp[!test] <- no[!test]; tmp) 
 
-attach(redfin_sub)
-redfin_sub$agecat[YearBuilt >= 1985] <- "Recent"
-redfin_sub$agecat[YearBuilt < 1945] <- "Historic"
-redfin_sub$agecat[YearBuilt >= 1945 & YearBuilt < 1985] <- "PostWar"
-detach(redfin_sub)
+attach(homes)
+homes$agecat[YearBuilt >= 1985] <- "Recent"
+homes$agecat[YearBuilt < 1945] <- "Historic"
+homes$agecat[YearBuilt >= 1945 & YearBuilt < 1985] <- "PostWar"
+detach(homes)
 
-#attach(redfin_sub)
-#redfin_sub$juriscat[City = "San Francisco"] <- "SF"
-#redfin_sub$juriscat[City != "San Francisco"] <- "Not SF"
-#detach(redfin_sub)
+#attach(homes)
+#homes$juriscat[City = "San Francisco"] <- "SF"
+#homes$juriscat[City != "San Francisco"] <- "Not SF"
+#detach(homes)
 
-hed1 <- lm(price ~ Sqft + agecat + juriscat, data=redfin_sub)
+hed1 <- lm(price ~ Sqft + agecat + juriscat, data=homes)
 summary(hed1)
+
+hed_cpi <- lm(price_api ~ Sqft + agecat + juriscat, data=homes)
+summary(hed_cpi)
+
+hed_hpi <- lm(price_hpi ~ Sqft + agecat + juriscat, data=homes, na.action="na.exclude")
+summary(hed_hpi)
+
+

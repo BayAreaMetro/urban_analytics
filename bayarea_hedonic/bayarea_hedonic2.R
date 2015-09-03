e99a7e7c2ac0911a6f5032c6bc10cdf6532e3635
Michael Reilly <mreilly@mtc.ca.gov>
2015-09-03 14:27:45 -0700

    add crime and refine

--- bayarea_hedonic/bayarea_hedonic2.R
+++ bayarea_hedonic/bayarea_hedonic2.R
@@ -1,6 +1,8 @@
 rf_raw = read.csv("./data/recent_housingsales_rf_1314_tazsub.csv", header = TRUE)
 rstr_raw = read.csv("./data/rastersamp12.txt", header = TRUE)
 market_segs = read.csv("./data/accessibility-market-segments.csv", header = TRUE)
+crime = read.csv("./data/crimerates.csv", header = TRUE)
+
 
 #inflation = read.csv("inflation_cycle_adjust.csv", header = TRUE)
 
@@ -47,16 +49,22 @@ ls10_nonMandatory$veryHighInc_autos_ge_workers_tn <- (ls10_nonMandatory$veryHigh
 
 ls10_nonMandatory$nwt <- 0.061 * ls10_nonMandatory$lowInc_0_autos_tn + 0.005 * ls10_nonMandatory$lowInc_autos_lt_workers_tn + 0.211 * ls10_nonMandatory$lowInc_autos_ge_workers_tn + 0.016 * ls10_nonMandatory$medInc_0_autos_tn + 0.013 * ls10_nonMandatory$medInc_autos_lt_workers_tn + 0.204 * ls10_nonMandatory$medInc_autos_ge_workers_tn + 0.009 * ls10_nonMandatory$highInc_0_autos_tn + 0.020 * ls10_nonMandatory$highInc_autos_lt_workers_tn + 0.208 * ls10_nonMandatory$highInc_autos_ge_workers_tn + 0.007 * ls10_nonMandatory$veryHighInc_0_autos_tn + 0.026 * ls10_nonMandatory$veryHighInc_autos_lt_workers_tn + 0.220 * ls10_nonMandatory$veryHighInc_autos_ge_workers_tn 
 
-
-
+summary(rf_raw)
+rf_raw$lot <- rf_raw$LotSize / 1
 
 levels(rf_raw$HomeType)
 
-rf_c <- subset(rf_raw, HomeType = c("Condo/Coop", "Single Family Residential", "Townhouse") &Saleyear > 2012 & YearBuilt > 0 & Sqft > 0 & taz_key > 0, select = c(HomeType, LastSalePr, Saleyear, City, Sqft, YearBuilt, Beds, taz_sub, OBJECTID))
+rf_c <- subset(rf_raw, Baths > 0 &Saleyear > 2012 & YearBuilt > 0 & Sqft > 0 & taz_key > 0, select = c(HomeType, LastSalePr, Saleyear, City, Sqft, YearBuilt, Beds, Baths, LotSize, taz_sub, Latitude, Longitude, OBJECTID))
+
+
+#rf_c <- subset(rf_raw, HomeType = c("Condo/Coop") &Saleyear > 2012 & YearBuilt > 0 & Sqft > 0 & taz_key > 0, select = c(HomeType, LastSalePr, Saleyear, City, Sqft, YearBuilt, Beds, Baths, LotSize, taz_sub, Latitude, Longitude, OBJECTID))
+rf_c <- subset(rf_raw, HomeType = c("Condo/Coop", "Single Family Residential", "Townhouse") & LotSize > 0 & Saleyear > 2012 & YearBuilt > 0 & Sqft > 0 & taz_key > 0 & LotSize > 0 , select = c(HomeType, LastSalePr, Saleyear, City, Sqft, YearBuilt, Beds, Baths, LotSize, taz_sub, Latitude, Longitude, OBJECTID))
 rstr_c <- subset(rstr_raw, NC10_901 > 0, select = c(HOUSING_SALES_RE, NC10_901, COASTDIS, HUD2K1, HUD2K_5KMISH, LYONSTEPSDIS, MAJRDDIS, KMTOPA, MEDINC2K_K1, OCEANVIEW, OPEN10KM, OPEN1KM, OPEN2POINT5KM, OPEN500M, OPEN5KM, STATIONDIS, BARTDIS, VIEW_WSW1))
 #infl <- subset(inflation, saleyear > 1990)
 
-homes_a <- merge (x = rf_c, y = rstr_c, by.x = "OBJECTID", by.y = "HOUSING_SALES_RE") 
+homes_raster <- merge (x = rf_c, y = rstr_c, by.x = "OBJECTID", by.y = "HOUSING_SALES_RE") 
+homes_a <- merge (x = homes_raster, y = crime, by.x = "City", by.y = "City") 
+
 homes_b <- merge (x = homes_a, y = ls10_mandatory, by.x = "taz_sub", by.y = "taz_sub", all.x = TRUE) 
 homes <- merge (x = homes_b, y = ls10_nonMandatory, by.x = "taz_sub", by.y = "taz_sub", all.x = TRUE) 
 
@@ -78,7 +86,7 @@ homes <- merge (x = homes_b, y = ls10_nonMandatory, by.x = "taz_sub", by.y = "ta
 #homes$price_hpi <- homes$LastSalePr * homes$adjHPIsfrsmt /100000
 
 homes$logprpersqft <-  log(homes$LastSalePr / homes$Sqft)
-#homes$price <- homes$LastSalePr / 100000
+homes$price <- homes$LastSalePr / 100000
 homes$juriscat <- ifelse(homes$City == "San Francisco", c("SF"), c("Not SF")) 
 #(tmp <- yes; tmp[!test] <- no[!test]; tmp) 
 
@@ -96,13 +104,18 @@ detach(homes)
 #detach(homes)
 
 homes_c <- subset(homes, logprpersqft > 0)
+homes_c$mwtnwt = homes_c$mwt * homes_c$nwt
 
+summary(homes_c)
 
 cor(homes_c$mwt, homes_c$nwt)
 
 
-hed1 <- lm(logprpersqft ~ Sqft + agecat + mwt + nwt  + HUD2K1 + MEDINC2K_K1 + OCEANVIEW , data=homes_c)
+hed1 <- lm(LastSalePr ~ Sqft + Beds + Baths + agecat + mwt   + HUD2K1 + MEDINC2K_K1 + OCEANVIEW + v_rate + p_rate , data=homes_c)
 summary(hed1)
+
+#hed1 <- lm(logprpersqft ~ Sqft + agecat + mwt + nwt  + HUD2K1 + MEDINC2K_K1 + OCEANVIEW , data=homes_c)
+#summary(hed1)
 
 
 # NC10_901, COASTDIS, HUD2K1, HUD2K_5KMISH, LYONSTEPSDIS, MAJRDDIS, KMTOPA, MEDINC2K_K1, OCEANVIEW, OPEN10KM, OPEN1KM, OPEN2POINT5KM, OPEN500M, OPEN5KM, STATIONDIS, BARTDIS, VIEW_WSW1

d8fc8d18a6c80cad5bab598c607b7936939d3363
Michael Reilly <mreilly@mtc.ca.gov>
2015-09-23 10:15:48 -0700

    try 5

--- pums/SAScii_import.R
+++ pums/SAScii_import.R
@@ -6,8 +6,8 @@ library(SAScii)
 #DOES NOT RUN without downloading ACS ASCII files to
 #your local drive from http://www.ipums.org/
 #MINNESOTA POPULATION CENTER - IPUMS ASCII EXTRACTS & SAS import instructions
-IPUMS.file.location <- "/Users/mkreilly/working/pums/IPUMS0812/usa_00002.dat"
-IPUMS.SAS.read.in.instructions <- "/Users/mkreilly/working/pums/IPUMS0812/usa_00002.sas"
+IPUMS.file.location <- "/Users/mkreilly/working/pums/IPUMS0812/usa_00005.dat"
+IPUMS.SAS.read.in.instructions <- "/Users/mkreilly/working/pums/IPUMS0812/usa_00005.sas"
 #store the IPUMS extract as an R data frame!
 pums0812_hh.df <- read.SAScii (IPUMS.file.location, IPUMS.SAS.read.in.instructions , zipped = F )
 #or store the IPUMS extract SAS import instructions for use in a
